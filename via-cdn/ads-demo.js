09f4c7bb8d3621674eab9cf2855ba09ad2f98bfe
hlhr202 <hlhr202@163.com>
2024-10-25 11:29:33 +0800

    init

---
+++ via-cdn/ads-demo.js
@@ -0,0 +1,35 @@
+const jolibox = new JoliboxSDK({
+  loaderConfig: {
+    testMode: true,
+  },
+});
+const { ads } = jolibox;
+ads.init({ testMode: true });
+
+// 在需要预加载广告的地方（例如在游戏加载屏幕中）
+ads.adConfig({
+  preloadAdBreaks: "on",
+  sound: "on",
+  onReady: () => {
+    console.log("onReady");
+  },
+});
+
+window.showAdBreak = () => {
+  // 在需要弹出奖励广告的地方
+  ads.adBreak({
+    type: "reward",
+    beforeReward(showAdFn) {
+      showAdFn();
+    },
+    adDismissed: () => {
+      console.log("adDismissed");
+    },
+    adViewed: () => {
+      console.log("adViewed");
+    },
+    adBreakDone: () => {
+      console.log("adBreakDone");
+    },
+  });
+};
---
+++ via-npm/src/main.ts
@@ -0,0 +1,40 @@
+import { JoliboxSDK } from "@jolibox/sdk";
+
+const jolibox = new JoliboxSDK({
+  loaderConfig: {
+    testMode: true,
+  },
+});
+const { ads } = jolibox;
+ads.init({ testMode: true });
+
+// 在需要预加载广告的地方（例如在游戏加载屏幕中）
+ads.adConfig({
+  preloadAdBreaks: "on",
+  sound: "on",
+  onReady: () => {
+    console.log("onReady");
+  },
+});
+
+const button = document.getElementById("ad-reward");
+if (button) {
+  button.addEventListener("click", () => {
+    // 在需要弹出奖励广告的地方
+    ads.adBreak({
+      type: "reward",
+      beforeReward(showAdFn) {
+        showAdFn();
+      },
+      adDismissed: () => {
+        console.log("adDismissed");
+      },
+      adViewed: () => {
+        console.log("adViewed");
+      },
+      adBreakDone: () => {
+        console.log("adBreakDone");
+      },
+    });
+  });
+}
