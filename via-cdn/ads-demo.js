acf16ab6cefa4d2ffa77d5770b20469030d9983d
hlhr202 <hlhr202@163.com>
2024-11-14 15:54:49 +0800

    update: force loader version

--- via-cdn/ads-demo.js
+++ via-cdn/ads-demo.js
@@ -1,11 +1,19 @@
-const jolibox = new JoliboxSDK();
+const jolibox = new JoliboxSDK({
+  loaderConfig: {
+    loaderMetadata: {
+      version: '1.0.4-beta.1',
+      asyncScriptUrl: 'https://cdn.jsdelivr.net/npm/@jolibox/web-async-sdk@1.0.4-beta.1/dist/index.iife.js',
+      syncScriptUrl: 'https://cdn.jsdelivr.net/npm/@jolibox/web-sync-sdk@1.0.4-beta.1/dist/index.iife.js',
+    }
+  }
+});
 const { ads } = jolibox;
 ads.init();
 
 // 在需要预加载广告的地方（例如在游戏加载屏幕中）
 ads.adConfig({
   preloadAdBreaks: "on",
-  sound: "on",
+  sound: "off",
   onReady: () => {
     console.log("onReady");
   },
